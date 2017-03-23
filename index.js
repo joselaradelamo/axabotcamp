0cd9abc4810e68cc7ffefded5631686847c453c4
Jose Lara <jose.lara.sopra@axa-groupsolutions.com>
2017-03-23 12:33:28 +0100

    Adding botAppId and botAppPassword

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const BotManager = require('flow-bot-manager').default;
 const restify = require('restify');
 
 var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function () {
-   console.log('%s listening to %s', server.name, server.url);
+server.listen(process.env.port || process.env.PORT || 3978, function() {
+  console.log('%s listening to %s', server.name, server.url);
 });
 
 var opts = {
-  defaultLocale: 'en',
+  botAppId: process.env.botAppId,
+  botAppPassword: process.env.botAppPassword,
+  defaultLocale: 'en-GB',
   localesPath: './bot/locales',
   cardPath: './bot/cards',
   actionPath: './bot/actions',
