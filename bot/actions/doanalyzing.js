cee9d6a34183e5aba6b42e0b817c878d218ea2b1
Luis Prieto <prieto.fdz@gmail.com>
2017-03-24 12:15:37 +0100

    meh

--- bot/actions/doanalyzing.js
+++ bot/actions/doanalyzing.js
@@ -1,19 +1,16 @@
 module.exports = function(session, args, next) {
   setTimeout(function() {
-    console.log('start settimeout');
     this.storage.getAllFromCollection('user', session.message.address.user.id, function (err, values) {
-      console.log(values);
       if (values.claimType && values.picture) {
         if (values.claimType === 'car') {
-          console.log('is a car');
           session.beginDialog('/showAnalysisCar');
-          next();
+          //next();
         } else if (values.claimType === 'building') {
           session.beginDialog('/showAnalysisBuilding');
-          next();
+          //next();
         } else {
           session.beginDialog('/showAnalysisOther');
-          next();
+          //next();
         }
       }
   });
