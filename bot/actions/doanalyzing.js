fde6c4d4faac3b661c64f550de577d93d4c8487e
Luis Prieto <prieto.fdz@gmail.com>
2017-03-24 11:38:57 +0100

    analysis each element

--- bot/actions/doanalyzing.js
+++ bot/actions/doanalyzing.js
@@ -1,5 +1,16 @@
 module.exports = function(session, args, next) {
   setTimeout(function() {
-    next();
+    this.storage.getAllFromCollection('user', session.message.address.user.id, function (err, values) {
+      if (values.claimType && values.picture) {
+        if (values.claimType === 'car') {
+          session.beginDialog('/showAnalysisCar');
+        } else if (values.claimType === 'building') {
+          session.beginDialog('/showAnalysisBuilding');
+        } else if (values.claimType === 'other') {
+          session.beginDialog('/showAnalysisOther');
+        }
+      }
+      next();
+  });
   }, 5000);
 }
