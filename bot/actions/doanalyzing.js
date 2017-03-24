c32ce980f7d82a2cdd110a0d3fa73b2488acf622
Luis Prieto <prieto.fdz@gmail.com>
2017-03-24 11:56:20 +0100

    car image url

--- bot/actions/doanalyzing.js
+++ bot/actions/doanalyzing.js
@@ -1,16 +1,21 @@
 module.exports = function(session, args, next) {
   setTimeout(function() {
+    console.log('start settimeout');
     this.storage.getAllFromCollection('user', session.message.address.user.id, function (err, values) {
+      console.log(values);
       if (values.claimType && values.picture) {
         if (values.claimType === 'car') {
+          console.log('is a car');
           session.beginDialog('/showAnalysisCar');
+          next();
         } else if (values.claimType === 'building') {
           session.beginDialog('/showAnalysisBuilding');
-        } else if (values.claimType === 'other') {
+          next();
+        } else {
           session.beginDialog('/showAnalysisOther');
+          next();
         }
       }
-      next();
   });
-  }, 5000);
+  }.bind(this), 5000);
 }
